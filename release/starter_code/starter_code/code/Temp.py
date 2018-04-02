1e7231465cca0553231b20e5f0c26d0cc7f445c0
Michael Luo <michael.luo@berkeley.edu>
2018-04-02 02:37:21 -0700

    Done except for camera position

--- release/starter_code/starter_code/code/Temp.py
+++ release/starter_code/starter_code/code/Temp.py
@@ -3,7 +3,7 @@ import cv2
 import scipy.io
 import matplotlib.pyplot as plt
 from matplotlib.lines import Line2D
-#from mpl_toolkits.mplot3d import Axes3D
+from mpl_toolkits.mplot3d import Axes3D
 #from IPython import embed
 
 '''
@@ -81,20 +81,100 @@ def find_rotation_translation(E):
     R_list = [-U.dot(W).dot(V), -U.dot(W.T).dot(V)]
     
     # Make sure the det(R)=1, !=-1
-    for i in R_list:
-        print(np.linalg.det(i))
+    #for i in R_list:
+        #print(np.linalg.det(i))
     #print(T_list)
     #print(R_list)
 
     return R_list, T_list
 
 def find_3d_points(P1, P2, matches):
-    print(P1)
-    print(P1.shape)
-    print(P2)
-    print(P2.shape)
+    solutions = []
+    error = 0.0
+
+    # Applying SVD to solve, LS solution doesn't seem to work? @ Alex
+    for i in range(0, matches.shape[0]):
+        x1, y1, x2, y2 = matches[i][0], matches[i][1], matches[i][2], matches[i][3]
+        X = np.array([x1*P1[2]-P1[0], y1*P1[2]-P1[1],x2*P2[2]-P2[0], y2*P2[2]-P2[1]])
+        U,S,V = np.linalg.svd(X)
+        svd_solution = V[V.shape[0]-1]/V[V.shape[0]-1][V.shape[1]-1]
+        solutions +=[svd_solution]
+
+        prediction1 = P1.dot(svd_solution.reshape((4, 1)))
+        prediction1/=prediction1[2][0]
+        prediction1 = prediction1.flatten()
+
+
+        prediction2 = P2.dot(svd_solution.reshape((4, 1)))
+        prediction2/=prediction2[2][0]
+        prediction2 = prediction2.flatten()
+        
+        error += ((prediction1[0] - x1)**2 + (prediction1[1] - y1)**2)**0.5
+        error += ((prediction2[0] - x2)**2 + (prediction2[1] - y2)**2)**0.5
+    solutions = np.array(solutions)
+    solutions = np.delete(solutions, 3, 1)
+    error /= 2*matches.shape[0]
+
+    #Applying least squares for every matching pair of points
+    '''
+    for i in range(0, matches.shape[0]):
+        
+        x1, y1, x2, y2 = matches[i][0], matches[i][1], matches[i][2], matches[i][3]
+        lhs = []
+        rhs = []
+        # x1, x2
+        lhs+= [[x1*P1[2][0]-P1[0][0], x1*P1[2][1] - P1[0][1], x1*P1[2][2]-P1[0][2]], [x2*P2[2][0]-P2[0][0], x2*P2[2][1] - P2[0][1], x2*P2[2][2]-P2[0][2]]]
+        rhs+= [[P1[0][3]-x1*P1[2][3]], [P2[0][3]-x2*P2[2][3]]]
+
+        # y1, y2
+        lhs+= [[y1*P1[2][0]-P1[1][0], y1*P1[2][1] - P1[1][1], y1*P1[2][2]-P1[1][2]], [y2*P2[2][0]-P2[1][0], y2*P2[2][1] - P2[1][1], y2*P2[2][2]-P2[2][1]]]
+        rhs+= [[P1[1][3]-y1*P1[2][3]], [P2[1][3]-y2*P2[2][3]]]
+        lhs = np.array(lhs)
+        rhs = np.array(rhs)
+        
+        ls_solution = np.array(np.linalg.inv(lhs.T.dot(lhs)).dot(lhs.T).dot(rhs))
+        solutions += [ls_solution]
+        #print(ls_solution)
+        prediction1 = P1.dot(np.insert(ls_solution, 3, 1, axis =0))
+        prediction1/=prediction1[2][0]
+        prediction1 = prediction1.flatten()
+        #print(prediction1)
+        #print(x1, y1)
+
+        prediction2 = P2.dot(np.insert(ls_solution, 3, 1, axis =0))
+        prediction2/=prediction2[2][0]
+        prediction2 = prediction2.flatten()
+        
+        #print(prediction2)
+        #print(x2, y2)
+        error += ((prediction1[0] - x1)**2 + (prediction1[1] - y1)**2)**0.5
+        error += ((prediction2[0] - x2)**2 + (prediction2[1] - y2)**2)**0.5
+    solutions = np.array(solutions).reshape((matches.shape[0], 3))
+    error/=2*matches.shape[0]
+    print(error)
+    '''
+    return (solutions,error)
+
+def plot_3d(P1, P2, points):
+    fig = plt.figure()
+    ax = plt.axes(projection='3d')
+
+    ax.set_xlim3d(min(np.min(points[:,0]), P1[0][3],P2[0][3] ), max(np.max(points[:,0]), P1[0][3],P2[0][3]))
+    ax.set_ylim3d(min(np.min(points[:,1]), P1[1][3],P2[1][3] ), max(np.max(points[:,1]), P1[1][3],P2[1][3]))
+    ax.set_zlim3d(min(np.min(points[:,2]), P1[2][3],P2[2][3] ), max(np.max(points[:,2]), P1[2][3],P2[2][3]))
+
+    ax.scatter3D(points[:,0], points[:,1], points[:,2],c=points[:,2], cmap='rainbow')
+    ax.scatter3D(P1[0][3], P1[1][3], P1[2][3],marker = '^');
+    ax.scatter3D(P2[0][3], P2[1][3], P2[2][3],marker = '^');
     
-    return (1,1)
+    ax.set_xlabel('X Label')
+    ax.set_ylabel('Y Label')
+    ax.set_zlabel('Z Label')
+
+    plt.show()
+
+
+    return 1
 
 
 def reconstruct_3d(name):
@@ -166,6 +246,7 @@ def reconstruct_3d(name):
             Z2 = (points_3d @ R2[2,:].T + t2[2])
             num_points[ti,ri] = np.sum(np.logical_and(Z1>0,Z2>0))
     (ti,ri) = np.where(num_points==np.max(num_points))
+
     j = 0 # pick one out the best combinations
     print(f"Reconstruction error = {errs[ti[j],ri[j]]}")
 
@@ -174,13 +255,13 @@ def reconstruct_3d(name):
     P2 = K2 @ np.concatenate([R2, t2[:, np.newaxis]], axis=1)
 
     # % compute the 3D points with the final P2
-    points, _ = find_3d_points() # <---------------------------------------------- You have already written this one!
+    points, _ = find_3d_points(P1, P2, matches) # <---------------------------------------------- You have already written this one!
 
     ## -------- plot points and centers of cameras ----------------------------
 
-    plot_3d() #<-------------------------------------------------------------- You write this one!
+    plot_3d(P1, P2, points) #<-------------------------------------------------------------- You write this one!
 
 
 # Main
-reconstruct_3d("house")
+reconstruct_3d("library")
 
